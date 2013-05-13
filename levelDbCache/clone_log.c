8f3979ed5d1a3fc37c84ab757437446b0124cbe5
rcs <andre.legendre@kalimasystems.org>
2013-05-13 18:21:01 +0200

    added copying notices and licence

--- levelDbCache/clone_log.c
+++ levelDbCache/clone_log.c
@@ -1,7 +1,28 @@
 /*  =========================================================================
 clone_log - Envoie des logs
-=========================================================================
-*/
+
+-------------------------------------------------------------------------
+Copyright (c) 1991-2013 Andre Charles Legendre <andre.legendre@kalimasystems.org>
+Copyright other contributors as noted in the AUTHORS file.
+
+This file is based on zeroMQ zguide examples <https://github.com/imatix/zguide> 
+
+This file is part of LevelDbCache, the shared in memory cache for levelDb Key Value store.
+
+This is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at
+your option) any later version.
+
+This software is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program. If not, see
+<http://www.gnu.org/licenses/>.
+*  ===================================================================== */
 
 #include "stdafx.h"
 #include <czmq.h>
